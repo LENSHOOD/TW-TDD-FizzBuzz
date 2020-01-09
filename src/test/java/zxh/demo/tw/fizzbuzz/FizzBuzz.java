8080e74b86753cfb85cdbeb0d7b8341817291cfb
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-01-10 00:16:01 +0800

    refactor: extract multiple of 3, 5 and 7

--- src/test/java/zxh/demo/tw/fizzbuzz/FizzBuzz.java
+++ src/test/java/zxh/demo/tw/fizzbuzz/FizzBuzz.java
@@ -7,22 +7,34 @@ package zxh.demo.tw.fizzbuzz;
 */
 public class FizzBuzz {
     public static String fizzBuzz(int input) {
-        if (input % 3 ==0 && input % 5 == 0) {
+        if (isMultipleOf3(input) && isMultipleOf5(input)) {
             return "FizzBuzz";
         }
 
-        if (input % 3 == 0) {
+        if (isMultipleOf3(input)) {
             return "Fizz";
         }
 
-        if (input % 5 == 0) {
+        if (isMultipleOf5(input)) {
             return "Buzz";
         }
 
-        if (input % 7 == 0) {
+        if (isMultipleOf7(input)) {
             return "Whizz";
         }
 
         return null;
+    }
+
+    private static boolean isMultipleOf7(int input) {
+        return input % 7 == 0;
+    }
+
+    private static boolean isMultipleOf5(int input) {
+        return input % 5 == 0;
+    }
+
+    private static boolean isMultipleOf3(int input) {
+        return input % 3 ==0;
     }
 }
