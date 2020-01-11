7f9b1b10a8ceac7fe97f4eda5283cf04c62c4c07
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-01-11 12:30:25 +0800

    feat: multiple of 3

---
+++ src/test/java/zxh/demo/tw/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+package zxh.demo.tw.fizzbuzz;
+
+/**
+ * FizzBuzz:
+ * @author zhangxuhai
+ * @date 2020/1/11
+*/
+public class FizzBuzz {
+    public static String fizzBuzz(int i) {
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
+
+        return null;
+    }
+}
---
+++ src/test/java/zxh/demo/tw/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package zxh.demo.tw.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * FizzBuzzTest:
+ * @author zhangxuhai
+ * @date 2020/1/11
+*/
+public class FizzBuzzTest {
+    @Test
+    public void should_return_fizz_when_multiple_3() {
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(6));
+    }
+}
