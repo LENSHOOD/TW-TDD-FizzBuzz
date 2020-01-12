d18a06fcefdd1ea68f37b992affcce2edface0ca
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-01-12 22:42:25 +0800

    feat: multiple of 3, 5, 7

---
+++ src/test/java/zxh/demo/tw/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,39 @@
+package zxh.demo.tw.fizzbuzz;
+
+/**
+ * FizzBuzz:
+ * @author zhangxuhai
+ * @date 2020/1/12
+*/
+public class FizzBuzz {
+
+    public static String fizzBuzz(int i) {
+        String result = "";
+
+        if (isMultipleOf3(i)) {
+            result += "Fizz";
+        }
+
+        if (isMultipleOf5(i)) {
+            result += "Buzz";
+        }
+
+        if (isMultipleOf7(i)) {
+            result += "Whizz";
+        }
+
+        return result;
+    }
+
+    private static boolean isMultipleOf7(int i) {
+        return i % 7 == 0;
+    }
+
+    private static boolean isMultipleOf5(int i) {
+        return i % 5 == 0;
+    }
+
+    private static boolean isMultipleOf3(int i) {
+        return i % 3 == 0;
+    }
+}
---
+++ src/test/java/zxh/demo/tw/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,28 @@
+package zxh.demo.tw.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * FizzBuzzTest:
+ * @author zhangxuhai
+ * @date 2020/1/12
+*/
+public class FizzBuzzTest {
+    @Test
+    public void should_return_fizz_when_multiple_3() {
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(6));
+    }
+
+    @Test
+    public void should_return_buzz_when_multiple_5() {
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(10));
+    }
+
+    @Test
+    public void should_return_whizz_when_multiple_7() {
+        assertEquals("Whizz", FizzBuzz.fizzBuzz(14));
+    }
+
+}
